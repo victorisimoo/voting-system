64702d06dfbd118b118a29f7e7c0f67478d9fd48
Victor Hernández <me@victorisimo.com>
2023-05-05 18:24:19 -0600

    I add aesthetic corrections to the project, in response to endpoints.

--- microservices/candidatesService.js
+++ microservices/candidatesService.js
@@ -7,7 +7,7 @@ let state = "createCandidates";
 
 app.post('/create', (req, res) => {
     if (state !== "createCandidates") {
-        return res.status(400).send('No se puede crear candidato en esta fase');
+        return res.status(400).send('[SERVER]: no se puede crear candidato en esta fase.');
     }
     const candidate = req.body;
     candidates.push(candidate);
@@ -18,16 +18,15 @@ app.get('/list', (req, res) => {
     res.status(200).send(candidates);
 });
 
-// Control de estado para este microservicio
 app.post('/admin/openCreate', (req, res) => {
     state = "createCandidates";
-    res.status(200).send('Fase de creación de candidatos abierta');
+    res.status(200).send('[SERVER]: ¡Fase de creación de candidatos abierta!');
 });
 
 app.post('/admin/closeCreate', (req, res) => {
     state = "closed";
-    res.status(200).send('Fase de creación de candidatos cerrada');
+    res.status(200).send('[SERVER]: ¡Fase de creación de candidatos cerrada!');
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Servicio de Candidatos corriendo en el puerto ${port}`));
+app.listen(port, () => console.log(`[SERVER]: servicio de candidatos corriendo en el puerto: ${port}`));
